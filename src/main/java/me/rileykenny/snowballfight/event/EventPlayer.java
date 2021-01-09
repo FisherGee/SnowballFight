40180196518e7f0b6ae96fae89b025b0a82b21e6
Kenny <confidential.kenny@gmail.com>
2021-01-09 10:43:49 -0800

    Clean up EventPlayer

--- src/main/java/me/rileykenny/snowballfight/event/EventPlayer.java
+++ src/main/java/me/rileykenny/snowballfight/event/EventPlayer.java
@@ -5,23 +5,20 @@ import org.bukkit.entity.Player;
 public class EventPlayer {
 
     EventManager.Team team;
-
-    //required to do bukkit stuff like teleport
-    Player bukkitPlayer;
-
-    public Player getBukkitPlayer(){
-        return bukkitPlayer;
-    }
-
-    public void setTeam(EventManager.Team team) {
-        this.team = team;
-    }
     private Player player;
     private int points;
 
     public EventPlayer(Player player, int points) {
         this.player = player;
         this.points = points;
+    }
+
+    public void setTeam(EventManager.Team team) {
+        this.team = team;
+    }
+
+    public EventManager.Team getTeam() {
+        return team;
     }
 
     public Player getPlayer() {
