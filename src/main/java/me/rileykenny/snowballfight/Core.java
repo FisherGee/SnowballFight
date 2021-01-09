62524c5e1185f0eb1664921d003b20af2db7e6d6
Kenny <confidential.kenny@gmail.com>
2021-01-09 10:37:37 -0800

    Added Basic EventQueue Functionality

--- src/main/java/me/rileykenny/snowballfight/Core.java
+++ src/main/java/me/rileykenny/snowballfight/Core.java
@@ -6,13 +6,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Core extends JavaPlugin {
 
     private EventManager eventManager;
+    private static JavaPlugin instance;
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         eventManager = new EventManager();
     }
 
     public EventManager getEventManager(){
         return eventManager;
+    }
+
+    public static JavaPlugin getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/me/rileykenny/snowballfight/event/EventQueue.java
@@ -0,0 +1,26 @@
+package me.rileykenny.snowballfight.event;
+
+import me.rileykenny.snowballfight.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class EventQueue {
+
+    private int seconds = 0;
+
+    public EventQueue(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void start() {
+        Bukkit.getScheduler().runTaskTimer(Core.getInstance(), () -> {
+            while (seconds != 0) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.sendMessage(ChatColor.YELLOW + Integer.toString(seconds) + " remaining!");
+                }
+            }
+        }, 0L, 20L);
+    }
+
+}
