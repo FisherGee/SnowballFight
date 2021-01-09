879ea1d8026a53f9a369666ec673fae6afe24873
Kenny <confidential.kenny@gmail.com>
2021-01-09 15:48:51 -0800

    Add default configs

---
+++ src/main/java/me/rileykenny/snowballfight/util/MessageUtil.java
@@ -0,0 +1,30 @@
+package me.rileykenny.snowballfight.util;
+
+import org.bukkit.ChatColor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageUtil {
+
+    public static String translate(String unTranslated) {
+        if (unTranslated != null) {
+            ChatColor.translateAlternateColorCodes('&', unTranslated);
+        }
+
+        return unTranslated;
+    }
+
+    public static ArrayList<String> translate(List<String> unTranslated) {
+        List<String> translated = new ArrayList<>();
+        if (unTranslated != null) {
+            for (String string : unTranslated) {
+                translated.add(ChatColor.translateAlternateColorCodes('&', string));
+            }
+        }
+
+        return (ArrayList<String>) unTranslated;
+    }
+
+
+}
